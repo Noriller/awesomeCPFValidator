1f1bed9e38113d2c8a8ecfb12652759bffd04a0e
Bruno Noriller <noriller@bb.com.br>
2020-10-23 14:01:51 -0300

    next app + ts + first tests

---
+++ src/components/validator/Validator.spec.tsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import Validator from './Validator';
+
+describe('Validator', () => {
+  it('should render a <div />', () => {
+    const wrapper = shallow(<Validator />);
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+});
+
+test('renders deploy link', () => {
+  const { getByText } = render(<Validator />);
+  const linkElement = getByText('Za Warudo');
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/components/validator/Validator.tsx
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+const Validator = () => <div className='app-container'>Za Warudo</div>;
+
+export default Validator;
